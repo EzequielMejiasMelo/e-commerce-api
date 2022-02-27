63b5a104663dfeda8adec6a9ac7311327347e26c
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-02-26 19:47:24 -0500

    Routing

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try{
+    const data = await Product.findAll({include: [{model: Tag, through: ProductTag}, {model: Category}]});
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try{
+    const data = await Tag.findByPk(req.params.id, {include: [{model: Tag, through: ProductTag}, {model: Category}]});
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
@@ -91,6 +101,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!data) {
+      res.status(404).json({message: 'No product with this ID'});
+      return;
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
